263c59a8aa80d91eca4298e4eb88e91507e25c66
Cristian <cdeveloperg@gmail.com>
2019-06-04 15:27:58 -0400

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Project
+                    Project GITHUB
                 </div>
 
                 <div class="links">
